2fbbdb6cdde316e6c6cdba6c54b4a4d33587bf83
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-06-28 20:01:08 -0400

    Add files via upload

---
+++ network-layer.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/network-layer', (req) => {
+    http.get(req.body.videosrc, function (res) {
+        res.on('data', (chunk) => {
+            console.log(retrieveStreamInfo(chunk))
+        });
+    }).on('error', (e) => {
+        console.log('Got error: ' + e.message);
+    });
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+function retrieveStreamInfo(data){
+    let str = data.toString().replace(/^\s+|\s+$/g, ''); //remove newlines at the end
+    let paragraphs = str.split("\n\n");
+    var streamInfo = [];
+    for (i = 0; i < paragraphs.length; i++){
+        const lines = paragraphs[i].split("\n");
+        const streamLocation = lines[lines.length-1];
+        const resolution = lines[lines.length-2].split('RESOLUTION=').pop().split(',')[0]
+        const qualityInfo = resolution + ": " + streamLocation;
+        streamInfo.push(qualityInfo);
+    }
+    return streamInfo;
+}
