45967284a21b4eec8999a215f9a5a67adce1376e
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-06-29 23:21:10 -0400

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 cd
-wget -q https://dl.google.com/go/go1.14.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.14.linux-amd64.tar.gz
-rm go1.14.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> /root/.bashrc
-echo 'export GOPATH=~/go' >> /root/.bashrc
-source /root/.bashrc
+wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+source ~/.profile
 
+sudo add-apt-repository ppa:jonathonf/ffmpeg-4
 sudo apt update
-sudo apt install ffmpeg
+sudo apt install ffmpeg -y
 ffmpeg -version
 
 sudo apt install nodejs
+npm install express
