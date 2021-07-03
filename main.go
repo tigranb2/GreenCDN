5f86383d56468a3bb66e5be3135110f0cea3b413
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-07-03 17:34:41 -0400

    removed nodejs

--- main.go
+++ main.go
@@ -2,25 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
+	"strings"
 )
 
 func main() {
-	arguments := os.Args
-	if len(arguments) < 2 {
-		fmt.Println("Please provide port...")
-		return
-	}
-
 	// configure the media directory name and port
 	const mediaDir = "media"
-	port, _ := strconv.Atoi(arguments[1])
+	port := 8000
 
 	// add a handler for the media files
-	http.Handle("/", addHeaders(http.FileServer(http.Dir(mediaDir))))
+	http.Handle("/", serveHandler(http.FileServer(http.Dir(mediaDir))))
+	http.Handle("/media-data", mediaDataHandler())
 	fmt.Printf("Starting server on %v\n", port)
 	log.Printf("Serving %s on HTTP port: %v\n", mediaDir, port)
 
@@ -28,10 +23,68 @@ func main() {
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
 
-// addHeaders will act as middleware to give us CORS support
-func addHeaders(h http.Handler) http.HandlerFunc {
+func serveHandler(h http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		h.ServeHTTP(w, r)
 	}
+}
+
+func mediaDataHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if r.Method == "POST"{
+			streamInfo := retrieveStreamInfo(r)
+			fmt.Println(streamInfo)
+		} else {
+			fmt.Println("HI!!")
+		}
+	}
+}
+
+func retrieveStreamInfo(r *http.Request) string {
+	// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+	if err := r.ParseForm(); err != nil {
+		return ""
+	}
+	videosrc := r.FormValue("videosrc")
+
+	res, err := http.Get(videosrc)
+	if err != nil {
+		return ""
+	}
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body) //read file
+	str := string(data)
+
+	str = strings.Join(strings.Split(str, "#EXT-X-STREAM-INF")[1:], "#EXT-X-STREAM-INF") //remove lines preceding quality level info
+	l := strings.Split(str, "\n")
+	var lines []string
+	for _, line := range l {
+		if line != "\n" && line != "" { //save only non-blank lines
+			lines = append(lines, line)
+		}
+	}
+
+	streamInfo := ""
+	for i := 0; i < len(lines); i += 2 {
+		resolution, _ := GetStringInBetweenTwoString(lines[i], "RESOLUTION=", ",")
+		source := lines[i+1]
+		streamInfo += resolution + ": " + source + "\n"
+	}
+	return streamInfo
+}
+
+func GetStringInBetweenTwoString(str string, startS string, endS string) (result string,found bool) {
+	s := strings.Index(str, startS)
+	if s == -1 {
+		return result,false
+	}
+	newS := str[s+len(startS):]
+	e := strings.Index(newS, endS)
+	if e == -1 {
+		return result,false
+	}
+	result = newS[:e]
+	return result,true
 }
--- setup.sh
+++ setup.sh
@@ -7,7 +7,3 @@ sudo add-apt-repository ppa:jonathonf/ffmpeg-4
 sudo apt update
 sudo apt install ffmpeg -y
 ffmpeg -version
-
-sudo apt install nodejs
-npm install express
-npm install --save cors
